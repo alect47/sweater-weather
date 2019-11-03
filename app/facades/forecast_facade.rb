1359aa1770d73f2e9d76db9cece46690d1201784
Alec Wells <alec.wells47@gmail.com>
2019-11-03 13:19:14 -0700

    Refator forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -8,11 +8,18 @@ class ForecastFacade
     Forecast.new(forecast_service, location)
   end
 
+
+private
+
   def forecast_service
-    DarkSkyService.new.get_forecast(location.latitude, location.longitude)
+    @forecast_service ||= DarkSkyService.new.get_forecast(location.latitude, location.longitude)
   end
 
   def location
-    LocationFacade.new(@location).location_info
+    @location_1 ||= Location.new(location_service)
+  end
+
+  def location_service
+    @location_service ||= LocationService.new.get_geocode(@location)
   end
 end
