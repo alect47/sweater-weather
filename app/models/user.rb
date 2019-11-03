6a946c3ff512ada259b9faaa83d58a9ca748d119
Alec Wells <alec.wells47@gmail.com>
2019-11-03 13:19:14 -0700

    Delete commented code

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  # validates_presence_of :api_key
 
   def api_key_generator
     SecureRandom.hex
