756fcbf9184eba91413966cd4cdd2db9a6512e94
Alec Wells <alec.wells47@gmail.com>
2019-11-03 13:19:14 -0700

    Update test coverage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,12 @@ describe User do
   describe 'validations' do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
-    # it {should validate_presence_of :api_key}
+  end
+
+  describe 'instance_methods' do
+    it 'api_key_generator' do
+      user = User.create(email: 'email@email.com')
+      expect(user.api_key_generator).to be_a String
+    end
   end
 end
