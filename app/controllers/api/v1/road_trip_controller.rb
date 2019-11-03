39dcfd9e26852ab1e9ccbdd95a184c63baf51c13
Alec Wells <alec.wells47@gmail.com>
2019-11-03 13:19:14 -0700

    Finish travel service

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::RoadTripController < ApplicationController
+  def create
+    # location = LocationFacade.new(params[:location]).location_info
+    # forecast = ForecastFacade.new(location).forecast
+    # render json: ForecastSerializer.new(forecast)
+  end
+end
