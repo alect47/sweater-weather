c0eb640e06bbe3a4b2770a6ba274df7fb3f82ffc
Alec Wells <alec.wells47@gmail.com>
2019-11-03 13:19:14 -0700

    Fix road_trip_controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
+    road_trip_params = json_parse(request)
     if User.find_by(api_key: road_trip_params[:api_key])
       road_trip = RoadTripFacade.new(road_trip_params).road_trip
       render json: RoadTripSerializer.new(road_trip)
@@ -11,7 +12,7 @@ class Api::V1::RoadTripController < ApplicationController
 
   private
 
-  def road_trip_params
-    params.require(:road_trip).permit(:api_key, :origin, :destination)
-  end
+  # def road_trip_params
+  #   params.require(:road_trip).permit(:api_key, :origin, :destination)
+  # end
 end
