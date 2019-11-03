690289345bee76d97616d6b90e8ee07eb9ef946e
Alec Wells <alec.wells47@gmail.com>
2019-11-03 14:37:57 -0700

    Make forecast controller and show action

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-
+    location = LocationFacade.new(params[:location]).location_info
+    forecast = ForecastFacade.new(location).forecast
+    render json: ForecastSerializer.new(forecast)
   end
 end
