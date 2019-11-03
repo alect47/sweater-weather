6536ea46cc3c5d53edb37151e5906a3ce337572b
Alec Wells <alec.wells47@gmail.com>
2019-11-03 13:19:14 -0700

    Add time to get_forecast ervice method

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -1,7 +1,11 @@
 class DarkSkyService
 
-  def get_forecast(lat, lng)
-    json_response = conn.get("#{lat},#{lng}")
+  def get_forecast(lat, lng, time = nil)
+    if time
+      json_response = conn.get("#{lat},#{lng},#{time}")
+    else
+      json_response = conn.get("#{lat},#{lng}")
+    end
     weather_data = JSON.parse(json_response.body, symbolize_names: true)
   end
 
--- spec/services/dark_sky_service_spec.rb
+++ spec/services/dark_sky_service_spec.rb
@@ -13,4 +13,21 @@ describe "Dark Sky api service" do
     expect(raw_data).to have_key(:hourly)
     expect(raw_data).to have_key(:daily)
   end
+
+  it "can get weather for location with timet" do
+    lat = "39.7392358"
+    lng = "-104.990251"
+    
+    time = 1573006147
+
+    service = DarkSkyService.new
+    raw_data = service.get_forecast(lat, lng, time)
+
+    expect(service).to be_a(DarkSkyService)
+    expect(raw_data).to be_a(Hash)
+    expect(raw_data).to have_key(:currently)
+    expect(raw_data[:currently][:time]).to eq(time)
+    expect(raw_data).to have_key(:hourly)
+    expect(raw_data).to have_key(:daily)
+  end
 end
