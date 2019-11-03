2ccfff2b7c9d2490c71e63b43f5025fb5299e59b
Alec Wells <alec.wells47@gmail.com>
2019-11-03 13:19:14 -0700

    Update test coverage for user error messages

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  
+
   def create
     user = User.new(user_params)
     user.update(api_key: user.api_key_generator)
--- spec/requests/api/v1/users/user_creates_account_spec.rb
+++ spec/requests/api/v1/users/user_creates_account_spec.rb
@@ -10,11 +10,24 @@ describe "User api" do
     user_info = '{"email": "email@example.com", "password": "password", "password_confirmation": "password"}'
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
     post "/api/v1/users", params: user_info, headers: headers
-    
+
     expect(response).to be_successful
 
     user_response = JSON.parse(response.body)
     expect(user_response["api_key"]).to eq("862e67f49fcf4be0cd49570633e97771")
+  end
+
+  it "user can send error messages" do
+    allow(SecureRandom)
+      .to receive(:hex)
+      .and_return("862e67f49fcf4be0cd49570633e97771")
+
+    user_info = '{"email": "", "password": "", "password_confirmation": ""}'
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    post "/api/v1/users", params: user_info, headers: headers
+
+    user_response = JSON.parse(response.body)
+    expect(user_response["errors"]).to eq("Invalid credentials: Password can't be blank and Email can't be blank")
   end
 end
 
