3f1804ed3cf28925ff75c90569a33e1d2e336b57
Alec Wells <alec.wells47@gmail.com>
2019-11-03 13:19:14 -0700

    Add road trip spec and route

---
+++ spec/requests/api/v1/road_trips/road_trip_forecast_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Road trip api' do
+  it 'user can get forecast for road trip' do
+    user = User.create!(email: 'alec@gmail.com', password: 'password', api_key: 'jgn983hy48thw9begh98h4539h4')
+    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    user_info = '{"origin": "Denver,CO", "destination": "Pueblo, CO", "api_key": "jgn983hy48thw9begh98h4539h4"}'
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    post "/api/v1/road_trip", params: user_info, headers: headers
+
+    expect(response).to be_successful
+    results = JSON.pase(response.body, symbolize_naems: true)
+
+    expect(results).to be_a Hash
+    expect(results[:data][:attributes][:distance]).to eg("223 mi")
+    expect(results[:data][:attributes][:travel_time]).to eg("223 mi")
+    expect(results[:data][:attributes][:forecast]).to be_a Hash
+    expect(results[:data][:attributes][:forecast][:temperature]).to eq("70")
+    expect(results[:data][:attributes][:forecast][:summary]).to eq("Partly Cloudy")
+  end
+end
