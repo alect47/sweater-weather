ad3f564c34b00ebb6b8b5ef02f24c825ef3a7e19
Alec Wells <alec.wells47@gmail.com>
2019-11-03 15:37:16 -0700

    Add today to forecast model

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,9 +1,14 @@
 class Forecast
-  attr_reader :address
+  attr_reader :id, :address, :currently, :hourly, :daily, :today
+
+  # think about refactoring this to make hash look different
+  # maybe add tonight?
   def initialize(forecast_hash, location)
+    @id = rand(0..999999)
     @currently = forecast_hash[:currently]
+    @today = forecast_hash[:daily][:data][0]
     @hourly = forecast_hash[:hourly]
-    @daily = forecast_hash[:daily]
+    @daily = forecast_hash[:daily][:data]
     @address = location.address
   end
 end
