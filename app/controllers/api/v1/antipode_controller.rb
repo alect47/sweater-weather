17a34c148e0b4b88580c7c2b2b12c388a3b5aa12
Alec Wells <alec.wells47@gmail.com>
2019-11-04 11:46:28 -0700

    Refactor antipode facade

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,10 +1,7 @@
 class Api::V1::AntipodeController < ApplicationController
   def show
     location = LocationFacade.new(params[:location]).location_info
-    # forecast = ForecastFacade.new(location).forecast
     antipode = AntipodeFacade.new(location).antipode
-    # images = background.background
-    # images.make_image_url
     render json: AntipodeSerializer.new(antipode)
   end
 end
--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -1,47 +1,37 @@
 class AntipodeFacade
-  attr_reader :location, :forecast
+  attr_reader :location
 
-  #need to also get antipode city name
   def initialize(location)
     @location = location
-    @forecast = forecast
   end
 
-  # def forecast
-  #   ForecastFacade.new(antipode_service).forecast
-  # end
+  def lat
+    antipode_service[:data][:attributes][:lat]
+  end
+
+  def long
+    antipode_service[:data][:attributes][:long]
+  end
 
   def antipode_city
-    lat = antipode_service[:data][:attributes][:lat]
-    long = antipode_service[:data][:attributes][:long]
     location = LocationService.new.get_address(lat, long)[:plus_code][:compound_code]
 
-    # LocationService.new.get_geocode(location)
     location_object = LocationFacade.new(location).location_info
   end
 
   def antipode_city_name
-    lat = antipode_service[:data][:attributes][:lat]
-    long = antipode_service[:data][:attributes][:long]
     location = LocationService.new.get_address(lat, long)[:plus_code][:compound_code]
-
   end
 
   def antipode_city_forecast
-    lat = antipode_service[:data][:attributes][:lat]
-    long = antipode_service[:data][:attributes][:long]
-
-    #this makes the dark sky hash which will need to be converted into Forecast object
-    # and passed to antipode model
-    # need to also make a location object
     ForecastFacade.new(antipode_city).forecast
-    # antipode_forecast = DarkSkyService.new.get_forecast(lat, long)
   end
 
   def antipode
     Antipode.new(antipode_city_forecast, antipode_city_name, @location)
   end
 
+private
   def antipode_service
     AntipodeService.new.get_antipode(@location.latitude, @location.longitude)
   end
