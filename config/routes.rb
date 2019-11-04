d749698d0daf4c264aa24cd6437f8fd66c791e92
Alec Wells <alec.wells47@gmail.com>
2019-11-03 18:11:02 -0700

    Add background route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       namespace :v1 do
         get '/forecast', to: 'forecast#show'
         get '/coordinates', to: 'location#show'
+        get '/backgrounds', to: 'background#show'
       end
     end
   end
