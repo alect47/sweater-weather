01773793faeea83c3f6d4e11b588b87fdcb77ffc
Alec Wells <alec.wells47@gmail.com>
2019-11-03 17:08:49 -0700

    Finish flickr service

---
+++ app/services/flickr_service.rb
@@ -0,0 +1,23 @@
+class FlickrService
+
+  def get_photos(lat, long)
+    json_response = conn.get('rest/?', {method: "flickr.photos.search",
+                                        tags: "parks,nature,skyline",
+                                        content_type: "1",
+                                        geo_context: "2",
+                                        lat: "#{lat}",
+                                        lon: "#{long}",
+                                        radius: "10",
+                                        format: "json",
+                                        nojsoncallback: "1",
+                                        })
+    photo_data = JSON.parse(json_response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(
+      url: 'https://www.flickr.com/services/',
+      params: {'api_key' => ENV['FLICKR_KEY']}
+    )
+  end
+end
