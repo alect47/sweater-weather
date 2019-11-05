c6b23a41aaa42b923b12c6a344a40d4e49cee8bf
Alec Wells <alec.wells47@gmail.com>
2019-11-05 11:33:57 -0700

    Add comments to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  #request.body.read
+  # JSON.parse(request.body.read, symbolize_names: true) maybe put
+  # in application controller
   def create
+    binding.pry
     user = User.new(user_params)
     user.update(api_key: user.api_key_generator)
     if user.save
