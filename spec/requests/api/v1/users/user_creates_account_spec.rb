319db7124941ffe8a273800578eaa36718163e2a
Alec Wells <alec.wells47@gmail.com>
2019-11-04 18:33:08 -0700

    Add user create account spec

---
+++ spec/requests/api/v1/users/user_creates_account_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "User api" do
+  it "user can send post requet and receive api key" do
+    allow(SecureRandom)
+      .to receive(:hex)
+      .and_return("862e67f49fcf4be0cd49570633e97771")
+
+    post "/api/v1/users?email=email@example.com&password=password&password_confirmation=password"
+
+    expect(response).to be_successful
+
+    forecast = JSON.parse(response.body)
+  end
+end
