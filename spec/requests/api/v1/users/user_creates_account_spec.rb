0a388271694d9501263ecc861505c1b99aecea07
Alec Wells <alec.wells47@gmail.com>
2019-11-05 11:33:45 -0700

    Change spec for user account creation

--- spec/requests/api/v1/users/user_creates_account_spec.rb
+++ spec/requests/api/v1/users/user_creates_account_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
 
 describe "User api" do
-  # you could clal it user ingo
-  # let(:user_attribute) {{email: password: password_confirmation}}
+
   it "user can send post requet and receive api key" do
     allow(SecureRandom)
       .to receive(:hex)
       .and_return("862e67f49fcf4be0cd49570633e97771")
 
-    post "/api/v1/users?email=email@example.com&password=password&password_confirmation=password"
-    # post user, not user
-    # params: user_attributes
+    user_info = '{"email": "email@example.com", "password": "password", "password_confirmation": "password"}'
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    post "/api/v1/users", params: user_info, headers: headers
+
     expect(response).to be_successful
+
     user_response = JSON.parse(response.body)
-    binding.pry
     expect(user_response["api_key"]).to eq("862e67f49fcf4be0cd49570633e97771")
   end
 end
+
+
+
+
+# you could call it user info
+# let(:user_attribute) {{email: password: password_confirmation}}
+
+# post user, not user
+# params: user_attributes
